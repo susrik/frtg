3853b5da7207103857308cfa34bcea4c74b867c8
Erik Reid <erik.reid@geant.org>
2020-03-21 12:43:05 +0100

    bumped release version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='frtg',
-    version="0.4.0",
+    version="0.5.0",
     author='Erik Reid',
     author_email='nobody@nowhere.org',
     description='Flask/React Twitter grid',
