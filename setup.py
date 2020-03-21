e4a19b9f67fca779468b965493b214fb7252037c
Erik Reid <erik.reid@geant.org>
2020-03-21 11:16:06 +0100

    added some pypi classifiers

--- setup.py
+++ setup.py
@@ -15,5 +15,14 @@ setup(
         'twython'
     ],
     include_package_data=True,
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Development Status :: 4 - Beta'
+    ],
+    python_requires='>=3.6'
 )
 
