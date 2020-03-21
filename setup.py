cce31aa9e8e21af64dbe255fdc98068b476e0d72
Erik Reid <erik.reid@geant.org>
2020-03-21 10:47:02 +0100

    use 3-digit semantic versioning

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='frtg',
-    version="0.4",
+    version="0.4.0",
     author='GEANT',
     author_email='swd@geant.org',
     description='Flask/React Twitter grid',
