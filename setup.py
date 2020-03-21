2e84be423841ee294839309acc0dfc302c9b3abc
Erik Reid <erik.reid@geant.org>
2020-03-21 16:13:26 +0100

    allow python v3.5

--- setup.py
+++ setup.py
@@ -22,12 +22,14 @@ setup(
     include_package_data=True,
     classifiers=[
         'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
         'Development Status :: 4 - Beta'
     ],
-    python_requires='>=3.6'
+    python_requires='>=3.5'
 )
 
